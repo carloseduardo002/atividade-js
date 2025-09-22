16d8d9e6577d09ad737ade951d80986c29297073
carloseduardo002 <eduafrat@gmail.com>
2025-09-22 01:21:45 -0300

    estudos do conceito basicos de JavaScript

---
+++ scripts.js
@@ -0,0 +1,52 @@
+// console.log("olá, console");
+
+// let num = 1; // para number sem aspas
+// let string = "Carlos"; // para String com aspas
+// let bool = true; // para valors em booleano sem aspas
+// let data = new Date = (2006/30/4); // estrutura de objeto
+
+
+// if e else
+let idade = 19;
+
+if (idade >= 18) {
+    console.log("você pode votar")
+} else {
+    console.log("você não pode votar")
+}
+
+
+// switch
+let dia = 3
+switch (dia){
+    case 1:
+        console.log("exemplo 1")
+        break;
+    case 2:
+        console.log('exemplo 2')
+        break;
+    case 3:
+        console.log('exemplo 3')
+        break;
+}
+
+
+// for, while, do while
+for (let a = 0; 
+    a < 5;
+    a++
+) {
+    console.log(a)
+}
+
+let b = 0;
+while (b < 5) {
+    console.log(b);
+    b++;
+}
+
+let c = 0;
+do {
+    console.log(c);
+    c++;
+} while (c < 5)
